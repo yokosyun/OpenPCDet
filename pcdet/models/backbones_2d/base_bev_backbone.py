c94c96e669ae2bb8f2d0ece73510241aff7e849f
yokosyun <yoko.syun.1408@gmail.com>
2025-03-29 21:32:53 +0900

    57-maskbatchnorm2d

--- pcdet/models/backbones_2d/base_bev_backbone.py
+++ pcdet/models/backbones_2d/base_bev_backbone.py
@@ -2,6 +2,15 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+def visualize_activation_histogram(bev_feat, title="activation_histogram"):
+    channels = bev_feat.size(1)
+    y = bev_feat.permute(1,0,2,3).reshape(channels, -1)
+    plt.title(title)
+    for i in range(channels):
+        plt.hist(y[i].cpu().detach().numpy(), bins=128, label=str(i))
+        plt.ylim(0, 1000)
+    plt.show()
 
 class BaseBEVBackbone(nn.Module):
     def __init__(self, model_cfg, input_channels):
@@ -34,13 +43,15 @@ class BaseBEVBackbone(nn.Module):
                     c_in_list[idx], num_filters[idx], kernel_size=3,
                     stride=layer_strides[idx], padding=0, bias=False
                 ),
-                nn.BatchNorm2d(num_filters[idx], eps=1e-3, momentum=0.01),
+                # nn.BatchNorm2d(num_filters[idx], eps=1e-3, momentum=0.01),
+                SparseBatchNorm2d(num_filters[idx], eps=1e-3, momentum=0.01),
                 nn.ReLU6()
             ]
             for k in range(layer_nums[idx]):
                 cur_layers.extend([
                     nn.Conv2d(num_filters[idx], num_filters[idx], kernel_size=3, padding=1, bias=False),
-                    nn.BatchNorm2d(num_filters[idx], eps=1e-3, momentum=0.01),
+                    # nn.BatchNorm2d(num_filters[idx], eps=1e-3, momentum=0.01),
+                    SparseBatchNorm2d(num_filters[idx], eps=1e-3, momentum=0.01),
                     nn.ReLU6()
                 ])
             self.blocks.append(nn.Sequential(*cur_layers))
@@ -78,6 +89,8 @@ class BaseBEVBackbone(nn.Module):
 
         self.num_bev_features = c_in
 
+        self.DEBUG = False
+
     def forward(self, data_dict):
         """
         Args:
@@ -89,9 +102,39 @@ class BaseBEVBackbone(nn.Module):
         ups = []
         ret_dict = {}
         x = spatial_features
-        for i in range(len(self.blocks)):
-            x = self.blocks[i](x)
 
+        masks = data_dict["masks"]
+        for i in range(len(self.blocks)):
+            # x = self.blocks[i](x)
+            block = self.blocks[i]
+            for layer in block:
+                if type(layer).__name__ in ["SparseBatchNorm2d", "BatchNorm2d", "InstanceNorm2d"]:
+                    if self.DEBUG:
+                        print(i, "------------", layer)
+                        bev_feat_reshape = x.reshape(-1, x.size(1))
+                        org_mean = bev_feat_reshape.mean(dim=0)
+                        org_var = bev_feat_reshape.var(dim=0)
+                        print(x.shape)
+                        print("org_mean", torch.max(org_mean), torch.min(org_mean))
+                        print("org_var", torch.max(org_var), torch.min(org_var))
+                        print("input=",torch.max(x), torch.min(x))
+                        # visualize_activation_histogram(x, type(layer).__name__ + str(i) + "_input")
+                    if type(layer).__name__ == "SparseBatchNorm2d":
+                        x = layer(x, masks)
+                    else:
+                        x = layer(x)
+                    if self.DEBUG:
+                        bev_feat_reshape = x.reshape(-1, x.size(1))
+                        norm_mean = bev_feat_reshape.mean(dim=0)
+                        norm_var = bev_feat_reshape.var(dim=0)
+                        print("norm_mean", torch.max(norm_mean), torch.min(norm_mean))
+                        print("norm_var", torch.max(norm_var), torch.min(norm_var))
+                        print("output=",torch.max(x), torch.min(x))
+                        # visualize_activation_histogram(x, type(layer).__name__ + str(i) + "_output")
+                else:
+                    x = layer(x)
+
+                
             stride = int(spatial_features.shape[2] / x.shape[2])
             ret_dict['spatial_features_%dx' % stride] = x
             if len(self.deblocks) > 0:
@@ -111,6 +154,57 @@ class BaseBEVBackbone(nn.Module):
 
         return data_dict
 
+from typing import Optional
+from torch import Tensor
+from torch.nn import functional as F 
+
+class SparseBatchNorm2d(nn.Module):
+    def __init__(
+        self,
+        num_features: int,
+        eps: float = 1e-5,
+        momentum: Optional[float] = 0.1,
+        affine: bool = True,
+        track_running_stats: bool = True,
+        device=None,
+        dtype=None,
+    ) -> None:
+        super(SparseBatchNorm2d, self).__init__()
+        self.bn = nn.BatchNorm2d(num_features, eps, momentum, affine, track_running_stats = True, device=device, dtype=dtype)
+        self.DEBUG = False
+
+    def forward(self, bev_feat: Tensor, masks: Tensor) -> Tensor:
+        if self.training:
+            with torch.no_grad():
+                kernel_size_h = masks.size(2) / bev_feat.size(2)
+                kernel_size_w = masks.size(3) / bev_feat.size(3)
+
+                assert kernel_size_h % 2 == 0, "not supported right now"
+
+                masks = torch.nn.functional.max_pool2d(masks.float(), kernel_size=int(kernel_size_h)).bool()
+
+                mask_expanded = masks.expand(-1, bev_feat.size(1), -1, -1)
+                mask_expanded = mask_expanded.permute(0, 2, 3, 1)
+                bev_feat_permute = bev_feat.permute(0, 2, 3, 1)
+                valid_feat = bev_feat_permute[mask_expanded]
+                valid_feat = valid_feat.reshape(-1, bev_feat.size(1))
+
+                mean = valid_feat.mean(dim=0)  # [C]
+                variance = valid_feat.var(dim=0) # [C]
+
+                self.bn.running_mean = self.bn.momentum * self.bn.running_mean + (1 - self.bn.momentum) * mean
+                self.bn.running_var = self.bn.momentum * self.bn.running_var + (1 - self.bn.momentum) * variance
+
+            
+            if self.DEBUG:
+                print("mask_mean", torch.max(mean), torch.min(mean))
+                print("mask_variance", torch.max(variance), torch.min(variance))
+
+        self.bn.track_running_stats = False
+        output = self.bn(bev_feat)
+        self.bn.track_running_stats = True
+
+        return output
 
 class BaseBEVBackboneV1(nn.Module):
     def __init__(self, model_cfg, **kwargs):
